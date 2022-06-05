02a0a04e25593437b2d296e8f75456d630151976
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-05 19:43:54 +0200

    ad 3 - start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,30 @@
 import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+
+const FirstDelayMs = document.querySelector('[name="delay"]');
+const delayStepMs = document.querySelector('[name="step"]');
+const amount = documenty.querySelector('[name="amount"]');
+
+const btnCreatePromises = document.querySelector('[type="submit"]');
+
+const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve('Success! Value passed to resolve function');
+//     } else {
+//       reject('Error! Error passed to reject function');
+//     }
+//   }, 2000);
+// });
+
+
+
+
+
+
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
