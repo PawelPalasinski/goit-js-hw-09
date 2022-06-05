d96ab00acb89488f56ea9fcd564f4d068ad56e3b
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-05 13:02:40 +0200

    ad 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,44 +1,85 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+
+const startBtn = document.querySelector('button[data-start]');
+const dateChosen = document.querySelector('#datetime-picker');
+const d = document.querySelector('[data-days]');
+const h = document.querySelector('[data-hours]');
+const m = document.querySelector('[data-minutes]');
+const s = document.querySelector('[data-seconds]');
+
+let timer = null;
+
+startBtn.disabled = true;
+
+//flatpickr
 
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
-  onClose(selectedDates) {
-    
-    const today = new Date();
-    if (selectedDates[0] <= today) {
+  onClose(selectedDate) {
+    const currentDate = new Date();
+    if (selectedDate[0] <= currentDate) {
+      startBtn.disabled = true;
       Notiflix.Notify.failure('Please choose a date in the future');
     } else {
-      Notiflix.Notify.success(
-        'The selected date is correct. Press the "start" button to continue.'
-      );
+      startBtn.disabled = false;
     }
   },
 };
 
-flatpickr('#datetime-picker', options);
+flatpickr(dateChosen, options);
 
+// counter
+
+startBtn.addEventListener('click', countdownTime);
+
+function countdownTime() {
+  timer = setInterval(() => {
+    startBtn.disabled = true;
+
+    const pickedDateMs = new Date(dateChosen.value).getTime();
+    const currentDateMs = new Date().getTime();
+    const timeLeft = pickedDateMs - currentDateMs;
+
+    const { days, hours, minutes, seconds } = convertMs(timeLeft);
+
+    d.innerHTML = String(days).length < 2 ? addLeadingZero(days) : days;
+    h.innerHTML =
+      String(hours).length < 2 ? addLeadingZero(hours) : hours;
+    m.innerHTML =
+      String(minutes).length < 2 ? addLeadingZero(minutes) : minutes;
+    s.innerHTML =
+      String(seconds).length < 2 ? addLeadingZero(seconds) : seconds;
+
+    if (timeLeft < 1000) {
+      clearInterval(timer);
+      startBtn.disabled = false;
+    }
+  }, 1000);
+}
+
+function addLeadingZero(value) {
+  const stringValue = String(value);
+  return stringValue.padStart(2, '0');
+}
+
+// function to convert time to ms
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
